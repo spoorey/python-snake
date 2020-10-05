a79756fd0ce202e06b2861fdaa8e6f442b06ed1f
David Spörri <spoerridavid@gmail.com>
2020-10-05 17:25:31 +0200

    add pycache to gitignore

---
+++ PlayerSnake.py
@@ -0,0 +1,21 @@
+class PlayerSnake:
+    direction='right'
+    headX = 10
+    headY = 10
+    bodyCoordinates = []
+    def reset():
+        PlayerSnake.bodyCoordinates=[]
+        PlayerSnake.headX = 10
+        PlayerSnake.headY = 10
+        PlayerSnake.move()
+
+    def move():
+        if (PlayerSnake.direction == 'right'):
+            PlayerSnake.headX += 1
+        elif (PlayerSnake.direction == 'left'):
+            PlayerSnake.headX -= 1
+        elif (PlayerSnake.direction == 'down'):
+            PlayerSnake.headY += 1
+        else:
+            PlayerSnake.headY -= 1
+        PlayerSnake.bodyCoordinates.append([PlayerSnake.headX, PlayerSnake.headY])
---
+++ main.py
@@ -0,0 +1,49 @@
+import tkinter
+
+from PlayerSnake import PlayerSnake
+
+top = tkinter.Tk()
+
+class GameSettings:
+    squareSideLength = 10
+    windowHeight = 1000
+    windowWidth = 1000
+
+def keyPress(event):
+    PlayerSnake.direction = event.keysym.lower()
+
+def fillSquare(canvas, x, y, color):
+    canvas.create_rectangle(
+        x*GameSettings.squareSideLength,
+        y*GameSettings.squareSideLength,
+        (x+1)*GameSettings.squareSideLength,
+        (y+1)*GameSettings.squareSideLength,
+        fill=color
+)
+
+def render(top, canvas):
+    canvas.delete("all")
+    top.after(200, render, top, canvas)
+
+    x = GameSettings.squareSideLength
+    y = GameSettings.squareSideLength
+
+    while (x < GameSettings.windowWidth):
+        canvas.create_line(x, 0, x, GameSettings.windowHeight)
+        x += GameSettings.squareSideLength
+
+    while (y < GameSettings.windowHeight):
+        canvas.create_line(0, y, GameSettings.windowWidth, y)
+        y += GameSettings.squareSideLength
+
+    for coordinate in PlayerSnake.bodyCoordinates:
+        fillSquare(canvas, coordinate[0], coordinate[1], "#0000ff")
+    PlayerSnake.move()
+
+PlayerSnake.reset()
+canvas = tkinter.Canvas(top, bg="#cccccc", height=GameSettings.windowHeight, width=GameSettings.windowWidth)
+canvas.pack()
+render(top, canvas)
+top.bind("<Key>", keyPress)
+top.mainloop()
+
